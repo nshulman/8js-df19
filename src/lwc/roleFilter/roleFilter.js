86dbe490319896d04d0c254dcfc6379ef6bcbe3c
nshulman <nathan@shulman.net>
2019-11-03 18:52:38 -0600

    added comment

--- src/lwc/roleFilter/roleFilter.js
+++ src/lwc/roleFilter/roleFilter.js
@@ -8,7 +8,7 @@ export default class RoleFilter extends LightningElement {
         event.target.variant = this.roles[event.target.className] ? "brand" : "";
 
         // DISPATCHEVENT AND CUSTOMEVENT
-        // Raise event with role values
+        // roleFilter.js: Raise event with role values
         const changeEvent = new CustomEvent('change', {detail: this.roles});
         this.dispatchEvent(changeEvent);
     }
